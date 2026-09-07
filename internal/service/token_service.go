cb7644d19210dddc24510ab05afb9da07d4c7972
aluka <88232045+a1uka@users.noreply.github.com>
2026-09-07 19:27:00 +0300

    laptop pc comm

--- internal/service/token_service.go
+++ internal/service/token_service.go
@@ -12,8 +12,8 @@ import (
 
 type TokenService interface {
 	Create(ctx context.Context, actorID uuid.UUID, token *model.MatchAPIToken) error
-	GetByID(ctx context.Context, tokenID uuid.UUID) (*model.MatchAPIToken, error)
-	GetByOrganisationID(ctx context.Context, organisationID uuid.UUID) ([]model.MatchAPIToken, error)
+	GetByID(ctx context.Context, actorID, tokenID uuid.UUID) (*model.MatchAPIToken, error)
+	GetByOrganisationID(ctx context.Context, organisationID uuid.UUID, includeInactive bool) ([]model.MatchAPIToken, error)
 	Update(ctx context.Context, actorID uuid.UUID, token *model.MatchAPIToken) error
 	Delete(ctx context.Context, actorID uuid.UUID, token *model.MatchAPIToken) error
 }
@@ -26,19 +26,51 @@ type tokenService struct {
 func NewTokenService(
 	txManager *database.TxManager,
 	tokenRepository repository.TokenRepository,
+	organisationRepository repository.OrganisationsRepository,
+	organisationMemberRepository repository.OrganisationMembersRepository,
 ) tokenService {
 	return &tokenService{
-		txManager:       txManager,
-		tokenRepository: tokenRepository,
+		txManager:                    txManager,
+		tokenRepository:              tokenRepository,
+		organisationRepository:       organisationRepository,
+		organisationMemberRepository: organisationMemberRepository,
 	}
 }
 
 // What should it do?
 // It should create a new db record with token
 // Should I create an Org table???
-func (s *tokenService) Create(ctx context.Context, actorID uuid.UUID, token *model.MatchAPIToken) error {
+func (s *tokenService) Create(ctx context.Context, actorID, organisationID uuid.UUID, token *model.MatchAPIToken) error {
 	return s.txManager.WithinTransaction(ctx, func(tx pgx.Tx) error {
 		tokenRepo := repository.NewTokensRepository(tx)
+		orgRepo := repository.NewOrganisationRepository(tx)
+
+		// Поменять потом isOwner на проверку разрешений роли
+		isOwner, err := orgRepo.IsOwner(ctx, organisationID, actorID)
+		if err != nil {
+			return err
+		}
+		if !isOwner {
+			return ErrForbidden
+		}
+		token.OrganisationID = &organisationID
+		return tokenRepo.Create(ctx, token)
+	})
+}
+
+func (s *tokenService) GetByID(ctx context.Context, tokenID uuid.UUID) (*model.MatchAPIToken, error) {
+	return s.tokenRepository.GetByID(ctx, tokenID)
+}
+
+func (s *tokenService) GetByOrganisationID(ctx context.Context, organisationID uuid.UUID, includeInactive bool) ([]model.MatchAPIToken, error) {
+	return s.tokenRepository.GetByOrganisation(ctx, organisationID, includeInactive)
+}
+
+func (s *tokenService) Update(ctx context.Context, actorID uuid.UUID, token *model.MatchAPIToken) error {
+	return s.txManager.WithinTransaction(ctx, func(tx pgx.Tx) error {
+		tokenRepo := repository.NewTokensRepository(tx)
+		orgRepo := repository.NewOrganisationRepository(tx)
+		orgMemberRepo := repository.NewOrganisationMembersRepository(tx)
 
 	})
 }
